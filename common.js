3de82670d6ca1e5e3579632cc78e2215705d8b3f
Илюза Гиззатуллина <Gizyaschwann@gmail.com>
2017-01-10 16:42:53 +0300

    courses structure

---
+++ common.js
@@ -0,0 +1,15 @@
+var str = "Halk Hogan",
+obj = {
+	prop: "yo",
+	fu: function () {
+
+	}
+},
+regexp = /baba/g,
+func = function(){},
+arr = [1,2,3];
+
+obj.prop = "heeey";
+
+console.log(str.toUpperCase());
+console.log(typeof str);
